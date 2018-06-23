1276f52833caf6f1e093071c1aee008f9e89083d
Aluno <Aluno@DOSO121756>
2018-06-23 13:04:25 -0300

    Funções cadastrar alunos e imprimir dados.

--- aulaspresenciais/ConsoleView/Program.cs
+++ aulaspresenciais/ConsoleView/Program.cs
@@ -10,6 +10,27 @@ namespace ConsoleView
     {
         static void Main(string[] args)
         {
+            Aluno a = CadastrarAluno();
+            ImprimirDados(a);
+
+            Console.ReadKey();
+        }
+
+        private static void ImprimirDados(Aluno a)
+        {
+            Console.WriteLine("Aluno : " + a.nome);
+            Console.WriteLine("Matricula: " + a.matricula);
+        }
+
+        private static Aluno CadastrarAluno()
+        {
+            Aluno a = new Aluno();
+
+            Console.Write("Digite o nome do aluno: ");
+            a.nome = Console.ReadLine();
+            Console.Write("Digite a matricula do aluno: ");
+            a.matricula = int.Parse(Console.ReadLine());
+            return a;
         }
     }
 }
