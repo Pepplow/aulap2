f8bba5fd47b00bd1b9a45fbb2bb5987b29482a1d
Aluno <Aluno@DOSO121773>
2018-07-07 15:58:01 -0300

    Lista funcionando

--- aulaspresenciais/WindowsFormsView1/TelaAluno/frmListaAluno.Designer.cs
+++ aulaspresenciais/WindowsFormsView1/TelaAluno/frmListaAluno.Designer.cs
@@ -29,33 +29,49 @@
         private void InitializeComponent()
         {
             this.dgvAlunos = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAlunos)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvAlunos
             // 
             this.dgvAlunos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvAlunos.Location = new System.Drawing.Point(107, 78);
+            this.dgvAlunos.Location = new System.Drawing.Point(70, 88);
             this.dgvAlunos.Name = "dgvAlunos";
-            this.dgvAlunos.Size = new System.Drawing.Size(399, 219);
+            this.dgvAlunos.Size = new System.Drawing.Size(339, 168);
             this.dgvAlunos.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(164, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "LISTA DE ALUNOS";
             // 
             // frmListaAluno
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(586, 409);
+            this.ClientSize = new System.Drawing.Size(484, 344);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dgvAlunos);
             this.Name = "frmListaAluno";
             this.Text = "frmListaAluno";
             this.Load += new System.EventHandler(this.frmListaAluno_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvAlunos)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvAlunos;
+        private System.Windows.Forms.Label label1;
     }
 }
